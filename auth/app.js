42bdc8771fcf8ef705304c0d4b14a7017a08aafa
Ahmed Elfakharany <a.elfakharany@activevideo.com>
2021-05-06 23:30:45 +0200

    Adds Redis support to the auth service

--- auth/app.js
+++ auth/app.js
@@ -1,43 +1,69 @@
+require('dotenv').config()
+const redis = require('redis');
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Hey, there! Welcome to this API service"
-  });
+app.use(express.json());
+const client = redis.createClient({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT });
+client.on('error', err => {
+    console.log('Error ' + err);
+    process.exit(1)
 });
-app.post("/api/posts", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err, authData) => {
-    if (err) {
-      res.sendStatus(403);
+exp = Math.floor(Date.now() / 1000) + parseInt(process.env.ACCESS_TOKEN_LIFE)
+// app.use(express.urlencoded({ extended: true }));
+// app.post("/", extractToken, (req, res) => {
+// jwt.verify(req.token, process.env.ACCESS_TOKEN_SECRET, (err, authData) => {
+//     if (err) {
+//       res.sendStatus(403);
+//     } else {
+//       res.json({
+//         authData,
+//       });
+//     }
+// });
+// });
+app.post("/login", (req, res) => {
+    if (req.body.username == "abohmeed" && req.body.password == "password") {
+        jwt.sign({
+            exp: exp,
+            user: {
+                username: req.body.username
+            }
+        }, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+            client.set(req.body.username, token, (err, reply) => {
+                if (err)
+                    throw err;
+            });
+            client.expireat(req.body.username, exp);
+            res.json({ token })
+        });
     } else {
-      res.json({
-        message: "POST created...",
-        authData
-      });
+        res.status(403).send('forbidden >_<')
     }
-  });
 });
-app.post("/api/login", (req, res) => {
-  const user = {
-    id: 1,
-    username: "john",
-    email: "john@gmail.com"
-  };
-  jwt.sign({ user: user }, "secretkey", (err, token) => {
-    res.json({
-      token
+// function extractToken(req, res, next) {
+// const bearerHeader = req.headers["authorization"];
+// if (typeof bearerHeader !== "undefined") {
+//     const bearerToken = bearerHeader.split(" ")[1];
+//     req.token = bearerToken;
+//     next();
+// } else {
+//     res.sendStatus(403);
+// }
+// }
+function saveToken(username, jwtToken) {
+    client.set(username, jwtToken, (err, reply) => {
+        if (err)
+            throw err;
+
+
+
+        console.log(reply);
     });
-  });
-});
-function verifyToken(req, res, next) {
-  const bearerHeader = req.headers["authorization"];
-  if (typeof bearerHeader !== "undefined") {
-    const bearerToken = bearerHeader.split(" ")[1];
-    req.token = bearerToken;
-    next();
-  } else {
-    res.sendStatus(403);
-  }
 }
+// client.get('foo', (err, reply) => {
+// if (err) throw err;
+// console.log(reply);
+// });
+
 app.listen(3000, () => console.log("Server started"));
