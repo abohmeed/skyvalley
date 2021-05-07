60f2b0b9293b17f89336667542d85dd9df4e5759
Ahmed Elfakharany <a.elfakharany@activevideo.com>
2021-05-08 01:17:44 +0200

    Cleans up the auth service

--- auth/app.js
+++ auth/app.js
@@ -10,18 +10,6 @@ client.on('error', err => {
     process.exit(1)
 });
 exp = Math.floor(Date.now() / 1000) + parseInt(process.env.ACCESS_TOKEN_LIFE)
-// app.use(express.urlencoded({ extended: true }));
-// app.post("/", extractToken, (req, res) => {
-// jwt.verify(req.token, process.env.ACCESS_TOKEN_SECRET, (err, authData) => {
-//     if (err) {
-//       res.sendStatus(403);
-//     } else {
-//       res.json({
-//         authData,
-//       });
-//     }
-// });
-// });
 app.post("/login", (req, res) => {
     if (req.body.username == "abohmeed" && req.body.password == "password") {
         jwt.sign({
@@ -38,32 +26,14 @@ app.post("/login", (req, res) => {
             res.json({ token })
         });
     } else {
-        res.status(403).json({"error":"Invalid username or password"})
+        res.status(403).json({ "error": "Invalid username or password" })
     }
 });
-// function extractToken(req, res, next) {
-// const bearerHeader = req.headers["authorization"];
-// if (typeof bearerHeader !== "undefined") {
-//     const bearerToken = bearerHeader.split(" ")[1];
-//     req.token = bearerToken;
-//     next();
-// } else {
-//     res.sendStatus(403);
-// }
-// }
 function saveToken(username, jwtToken) {
     client.set(username, jwtToken, (err, reply) => {
         if (err)
             throw err;
-
-
-
         console.log(reply);
     });
 }
-// client.get('foo', (err, reply) => {
-// if (err) throw err;
-// console.log(reply);
-// });
-
 app.listen(3000, () => console.log("Server started"));
